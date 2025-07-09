4003c9ae7e06267b09a05fc91f39004883217ae4
JASONGAME1 <106131202+JASONGAME1@users.noreply.github.com>
2025-07-09 23:15:33 +0800

    Add files via upload

---
+++ day_9.py
@@ -0,0 +1,9 @@
+'''for i in range(1,10):
+    for j in range(1,10):
+        print(f"{i}x{j}={i*j}",end="\t")
+    print()    
+'''
+for i in range(10,1,-1):
+    for j in range(10,1,-1):
+        print(f"{i}x{j}={i*j}",end="\t")
+    print()        
