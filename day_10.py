18c60e2d4e1ac6cd5c57a639cf8f33cfd97f5165
JASONGAME1 <106131202+JASONGAME1@users.noreply.github.com>
2025-07-10 21:48:13 +0800

    Add files via upload

---
+++ day_10.py
@@ -0,0 +1,11 @@
+total = 0
+for i in range(1,101):
+    if i %2 ==0:
+        total += i
+print("1-100的偶數和為:",total)
+
+
+total1 = 0
+for i in range(1,101,2):
+ total1 += i
+print("1-100的基數合為:",total1)    
